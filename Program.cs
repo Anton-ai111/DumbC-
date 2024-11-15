4a4841f1a56ef2b5e3d6e0dd2ff58451c2f1b063
Anton Hrlić <138965551+Anton-ai111@users.noreply.github.com>
2024-11-15 10:57:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number to divide by:");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The result of dividing {0} by {1} is {2}", number
+            , divisor, number / divisor);
+        }
+    }
+}
